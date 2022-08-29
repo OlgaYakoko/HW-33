520f2bb42faaca9586e263d5ef7661958e1b0d96
OlgaYakoko <108624217+OlgaYakoko@users.noreply.github.com>
2022-08-29 17:07:34 +0100

    hello

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+void function () {
+
+const ladder = {
+    step: 0,
+    up: function() {
+      this.step++;
+     return this;
+    },
+    down: function() {
+      this.step--;
+      return this;
+    },
+    showStep: function() { 
+      alert( this.step );
+    }
+  };
+
+ladder.up().up().down().showStep();
+
+}();
